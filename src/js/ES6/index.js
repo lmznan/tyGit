76df374991c3f72b01d6e95e44adf75f9a33f551
lmznan <596420902@qq.com>
2019-08-31 22:16:46 +0800

    load头部尾部，布局轮播结构

--- src/js/ES6/index.js
+++ src/js/ES6/index.js
@@ -2,9 +2,25 @@ $(function(){
     //导入头部
     var $head = $('#headerLink');
     $head.load('public.html #headerWrap',function(){
-
-    })
+        var $ClassicSuit = $('.header-nav ul li').eq(1);
+        var $HeadSubmenu = $('.header-submenu');
+        var $shopping = $('.header-nav ul > li').last();
+        var $cart = $('.shop-con');
+        $ClassicSuit.hover(function(){
+            $HeadSubmenu.css('display','block');
+        },function(){
+            $HeadSubmenu.css('display','none');
+        })
+        $shopping.hover(function(){
+            $cart.css('display','block');
+        },function(){
+            $cart.css('display','none');
+        })
+    });
     //导入脚部
-    var $footer = $('footerLink');
+    var $footer = $('#footerLink');
+    $footer.load('public.html #footerWrap');
+    /* 大图轮播 */
+    $banBox = $('.bannerBox');
 })
 
