0bb7085644c42afefc2c67369e49a29671864e66
lmznan <596420902@qq.com>
2019-08-31 09:28:01 +0800

    初始项目

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+//1.导入所需模块
+let gulp = require('gulp'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    cssnano = require('gulp-cssnano'),
+    imagemin = require('gulp-imagemin'),
+    sass = require('gulp-sass');
+    babel = require('gulp-babel');
+
+//2.发布任务 task('任务名'，回调函数)
+//发布测试任务
+gulp.task('test',()=>{
+    console.log('gulp测试成功！');
+})
+
+gulp.task('sass',()=>{
+    gulp.src('./src/sass/*.scss').pipe(sass())
+    //.pipe(cssnano())
+    .pipe(rename({suffix : '.min'})).pipe(gulp.dest('./dist/css'));
+})
+gulp.task('img', () => {
+    gulp.src('./src/img/*.*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+})
+gulp.task('es6',()=>{
+    gulp.src('./src/js/ES6/*.js')
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
+    .pipe(gulp.dest('./src/js/ES5'));
+})
+gulp.task('js',()=>{
+    gulp.src('./src/js/ES5/*.js')
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./dist/js'));
+})
+gulp.task('default',()=>{
+    gulp.watch('./src/sass/*.scss',['sass']);
+})
